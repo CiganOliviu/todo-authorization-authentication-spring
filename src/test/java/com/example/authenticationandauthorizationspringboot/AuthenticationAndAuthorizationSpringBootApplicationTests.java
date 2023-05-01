3fc4e89d6829ec31a6b5498774898b727b3c35e9
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-05-01 15:36:15 +0300

    Updates

---
+++ src/test/java/com/example/authenticationandauthorizationspringboot/AuthenticationAndAuthorizationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenticationandauthorizationspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAndAuthorizationSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
