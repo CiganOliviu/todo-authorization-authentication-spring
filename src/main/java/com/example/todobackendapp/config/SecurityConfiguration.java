f4e8451e02135c372227adca3ddbfc9e04429f4d
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-05-19 17:24:31 +0300

    Updates

--- src/main/java/com/example/todobackendapp/config/SecurityConfiguration.java
+++ src/main/java/com/example/todobackendapp/config/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().
                 disable().
-                authorizeHttpRequests().requestMatchers("https://localhost:3000/").permitAll().
+                authorizeHttpRequests().requestMatchers("/api/custom-auth/**").permitAll().
                 anyRequest().authenticated().and().sessionManagement().
                 sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
                 authenticationProvider(authenticationProvider).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
