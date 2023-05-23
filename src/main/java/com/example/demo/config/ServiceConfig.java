a14b534f795752c2476041eb677895b3ed82b84b
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-05-24 00:56:17 +0300

    Updates

--- src/main/java/com/example/demo/config/ServiceConfig.java
+++ src/main/java/com/example/demo/config/ServiceConfig.java
@@ -3,7 +3,6 @@ package com.example.demo.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
